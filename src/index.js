ba415ed73c73087ff7499419dd6268162273d34a
Dormeh <dormeh@icloud.com>
2022-04-05 19:16:38 +0300

    index.js URL fix

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import Router from './router/index.js';
 import tooltip from './components/tooltip/index.js';
 
 tooltip.initialize();
+const URL_PATH = process.env.URL_PATH;
 
 const router = Router.instance();
 
 router
-  .addRoute(/^$/, 'dashboard')
-  .addRoute(/^products$/, 'products/list')
-  .addRoute(/^products\/add$/, 'products/edit')
-  .addRoute(/^products\/([\w()-]+)$/, 'products/edit')
-  .addRoute(/^sales$/, 'sales')
-  .addRoute(/^categories$/, 'categories')
+  .addRoute(new RegExp(`^${URL_PATH}$`), 'dashboard')
+  .addRoute(new RegExp(`^${URL_PATH}products$`), 'products/list')
+  .addRoute(new RegExp(`^${URL_PATH}products/add$`), 'products/edit')
+  .addRoute(new RegExp(`^${URL_PATH}products/([\\w()-]+)$`), 'products/edit')
+  .addRoute(new RegExp(`^${URL_PATH}sales$`), 'sales')
+  .addRoute(new RegExp(`^${URL_PATH}categories$`), 'categories')
   .addRoute(/^404\/?$/, 'error404')
   .setNotFoundPagePath('error404')
   .listen();
